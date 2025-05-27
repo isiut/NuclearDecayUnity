d44b10190c1a8101ea0418ffdf812e28970db8c9
Illia Siutkin <illiasiutkin07@gmail.com>
2025-05-26 19:08:12 -0500

    Create nucleus spawning

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,40 @@
+using System.Threading;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Attractor : MonoBehaviour
+{
+    [Tooltip("Strength of the force pulling the nucleons toward the origin.")]
+    public float attractionForce = 10f;
+
+    public float settleTime = 3f;
+    float startTime;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startTime = Time.time;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 directionToCenter = -transform.position; // Toward (0,0,0)
+        float distance = directionToCenter.magnitude;
+
+        float elapsedTime = Time.time - startTime;
+
+        if (elapsedTime - startTime < settleTime)
+        {
+            // Apply force toward center
+            Vector3 force = directionToCenter.normalized * attractionForce;
+            rb.AddForce(force, ForceMode.Acceleration);
+        }
+        else
+        {
+            // Optionally dampen velocity to settle at center
+            rb.linearVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour
+{
+    [Tooltip("Number of nucleons to spawn.")]
+    public int numberOfProtons = 20;
+    public int numberOfNeutrons = 20;
+
+    public GameObject protonPrefab;
+    public GameObject neutronPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int protonsSpawned = 0;
+        int neutronsSpawned = 0;
+        int totalNucleons = numberOfProtons + numberOfNeutrons;
+
+        for (int i = 0; i < totalNucleons; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+
+            // Alternate spawning, but ensure we don't exceed the desired count for each type
+            if ((i % 2 == 0 && protonsSpawned < numberOfProtons) || neutronsSpawned >= numberOfNeutrons)
+            {
+                GameObject proton = Instantiate(protonPrefab, position, Quaternion.identity);
+                proton.transform.localScale = Vector3.one * 0.5f;
+                protonsSpawned++;
+            }
+            else
+            {
+                Instantiate(neutronPrefab, position, Quaternion.identity);
+                neutronPrefab.transform.localScale = Vector3.one * 0.5f;
+                neutronsSpawned++;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
